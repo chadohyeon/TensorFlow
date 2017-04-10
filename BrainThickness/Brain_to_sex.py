175b4f15d0824357166bfa26b934e2756ea7ae88
DOHYEON CHA <eric1@naver.com>
2017-04-11 06:37:01 +0900

    Update Brain_to_sex.py

--- BrainThickness/Brain_to_sex.py
+++ BrainThickness/Brain_to_sex.py
@@ -3,12 +3,13 @@ import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 path="C:/Users/cha/Desktop/brain/"
 
-def logireg (X1,Y1,X2,Y2, lr,epoch, dr, batch_size):
+def logireg (X1,Y1,X2,Y2, lr,epoch, dr, batch_size, filename, decaying_rate):
     cols=X1.shape[1]
     train_n=X1.shape[0]
     x=tf.placeholder(tf.float32, shape=[None,cols])
     y=tf.placeholder(tf.float32, shape=[None,1])
     dropout_rate=tf.placeholder(tf.float32)
+    decaying_lr=tf.placeholder(tf.float32)
 
     # 3 hidden layers
     w1=tf.get_variable('w1',shape=[cols,256], initializer=tf.contrib.layers.xavier_initializer())
@@ -33,7 +34,7 @@ def logireg (X1,Y1,X2,Y2, lr,epoch, dr, batch_size):
 
     # Using Adam optimizer
     cost=-tf.reduce_mean(y*tf.log(hypothesis)+(1-y)*tf.log(1-hypothesis))
-    train=tf.train.AdamOptimizer(learning_rate=lr).minimize(cost)
+    train=tf.train.AdamOptimizer(learning_rate=decaying_lr).minimize(cost)
 
     predicted=tf.cast(hypothesis>0.5, dtype=tf.float32)
     accuracy=tf.reduce_mean(tf.cast(tf.equal(predicted, y), dtype=tf.float32))
@@ -65,27 +66,28 @@ def logireg (X1,Y1,X2,Y2, lr,epoch, dr, batch_size):
                 else:
                     batch_x=X1[j*batch_size:,:]
                     batch_y=Y1[j*batch_size:,:]
-                cost_val, _ = sess.run([cost,train], feed_dict={x: batch_x, y: batch_y, dropout_rate:dr})
-            # Showing loss values
+                cost_val, _ = sess.run([cost,train], feed_dict={x: batch_x, y: batch_y, dropout_rate:dr, decaying_lr: lr})
+            # Showing loss values with decaying learning rate
             if (step+1)%100==0:
                 print(step+1,cost_val)
+                saver.save(sess, path+filename)
+                lr=lr*decaying_rate**((step+1)/100)
             # Showing interim accuracy
             if (step+1)%500==0:
-                mid_h, mid_c, mid_a = sess.run([hypothesis, predicted, accuracy], feed_dict={x: X2, y: Y2, dropout_rate: 1.0})
-                print("\n Hypothesis: ", mid_h, "\nCorrect (Y) ", mid_c, "\nAccuracy: ", mid_a)
+                mid_a = sess.run([accuracy], feed_dict={x: X2, y: Y2, dropout_rate: 1.0})
+                print("Accuracy: ", mid_a)
             # Break if loss is NaN
             if np.isnan(cost_val):
                 # Exit the function if loss is NaN at early stage
                 if step+1 < 1000:
                     print("Use lower learning rate")
                     return 0
-                print ("Total steps:",step+1)
+                print ("Total steps: ", step+1)
                 break
-        saver.save(sess,path+"brain-sex")
+        saver.save(sess,path+filename)
         # accuracy report
         h,c,a=sess.run([hypothesis, predicted, accuracy], feed_dict={x:X2, y:Y2, dropout_rate:1.0})
         print("\n Hypothesis: ", h, "\nCorrect (Y) ", c, "\nAccuracy: ", a)
-
 #x=scaler.fit_transform(np.loadtxt(path+'hcpThick731.csv', delimiter=",", dtype=np.float32))
 x=np.loadtxt(path+'hcpThick731.csv', delimiter=",", dtype=np.float32)
 sex=np.loadtxt(path+'hcpGender.csv', delimiter=",", dtype=np.float32)
@@ -95,4 +97,7 @@ train_y=np.array([gender]).T[0:657,:]
 test_x=x.T[657:,:]
 test_y=np.array([gender]).T[657:,:]
 
-logireg(train_x, train_y, test_x, test_y,lr=0.000001,epoch=20000,dr=0.7, batch_size=200)
+logireg(train_x, train_y, test_x, test_y,lr=1e-6,epoch=20000,dr=0.7, batch_size=200, filename="Gender_1e-6", decaying_rate=0.99)
+
+
+
